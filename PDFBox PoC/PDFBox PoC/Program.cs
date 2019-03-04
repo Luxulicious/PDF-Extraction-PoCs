3c62898af8e6a595872e29c385872df6d60507a6
Lux <9366973+Luxulicious@users.noreply.github.com>
2019-03-04 13:48:57 +0100

    PDFBox basic text extraction

---
+++ PDFBox PoC/PDFBox PoC/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using java.util;
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+
+namespace PDFBox_PoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path =
+                "C:/Users/t.ruijs/Documents/GitHub/PDF-Extraction-PoCs/PDFBox PoC/Example PDFs/sample.pdf";
+            var text = ExtractTextFromPdf(path);
+            Console.WriteLine("Extracted text:\n\n" + text + "\n\n");
+            //Console.WriteLine(GetPDDocument(path).getDocumentCatalog());
+
+            Something(path);
+            var key = Console.ReadKey();
+        }
+
+        private static string ExtractTextFromPdf(string path)
+        {
+            PDDocument doc = null;
+            try
+            {
+                doc = PDDocument.load(path);
+                PDFTextStripper stripper = new PDFTextStripper();
+                return stripper.getText(doc);
+            }
+            finally
+            {
+                if (doc != null)
+                {
+                    doc.close();
+                }
+            }
+        }
+
+        private static void Something(string path)
+        {
+            var doc = GetPDDocument(path);
+            PDFTextExtractor pdfTextExtractor = new PDFTextExtractor();
+            Console.WriteLine(pdfTextExtractor.getText(doc));
+        }
+
+        private static PDDocument GetPDDocument(string path)
+        {
+            PDDocument doc = null;
+            try
+            {
+                doc = PDDocument.load(path);
+                return doc;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
+    }
+}
+
+public class PDFTextExtractor : PDFTextStripper
+{
+    protected override void writeString(string str, List textPositions)
+    {
+        Iterator it = textPositions.iterator();
+        while (it.hasNext())
+        {
+            var text = (TextPosition) it.next();
+            Console.WriteLine(
+                "["
+                + "character=" + text.getCharacter() + " DirAdj="
+                + text.getXDirAdj() + "," +
+                text.getYDirAdj() + " fs=" + text.getFontSize() + " xscale=" +
+                text.getXScale() + " height=" + text.getHeightDir() + " space=" +
+                text.getWidthOfSpace() + " width="
+                + "]");
+        }
+    }
+}
