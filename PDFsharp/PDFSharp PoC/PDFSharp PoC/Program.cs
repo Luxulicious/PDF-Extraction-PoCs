fc8eb1a834919a5bff57ed63ebc34c8694144957
Lux <9366973+Luxulicious@users.noreply.github.com>
2019-03-06 14:51:15 +0100

    Tried out PDFSharp one last time
    But alas it was not meant to be...

--- PDFsharp/PDFSharp PoC/PDFSharp PoC/Program.cs
+++ PDFsharp/PDFSharp PoC/PDFSharp PoC/Program.cs
@@ -20,16 +20,48 @@ namespace HelloWorld
     /// </summary>
     class Program
     {
-        public static string defaultPath = "C:/Users/t.ruijs/Documents/GitHub/PDF-Extraction-PoCs/Example PDFs/DeleteMe.pdf";
+        public static string defaultPath = "C:/Users/t.ruijs/Documents/GitHub/PDF-Extraction-PoCs/Example PDFs/factuur 2018-0137.pdf";
 
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Native extraction:");
+            /*Console.WriteLine("Native extraction:");
             WriteTextUsingPdfSharpNative();
             Console.WriteLine("BuildTablesFromPdf extraction:");
             WriteTextUsingBuildTablesFromPdf();
+            Console.WriteLine(PdfTextExtractor.GetText(defaultPath));*/
+            PDFParser();
             Console.ReadKey();
+            
+        }
+
+        public static void PDFParser()
+        {
+            var streamWriter = new StreamWriter("output.txt", false);
+
+            String outputText = "";
+
+            try
+            {
+                PdfDocument inputDocument = PdfReader.Open(defaultPath, PdfDocumentOpenMode.ReadOnly);
+
+                foreach (PdfPage page in inputDocument.Pages)
+                {
+                    for (int index = 0; index < page.Contents.Elements.Count; index++)
+                    {
+                        PdfDictionary.PdfStream stream = page.Contents.Elements.GetDictionary(index).Stream;
+                        outputText = new PDFTextExtractor().ExtractTextFromPDFBytes(stream.Value);
+
+                        streamWriter.WriteLine(outputText);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            streamWriter.Close();
         }
 
         private static void WriteTextUsingBuildTablesFromPdf()
@@ -176,6 +208,195 @@ namespace HelloWorld
         private static void ExtractText(CString obj, StringBuilder target)
         {
             target.Append(obj.Value);
+        }
+        #endregion
+    }
+
+    public class PDFTextExtractor
+    {
+        /// BT = Beginning of a text object operator 
+        /// ET = End of a text object operator
+        /// Td move to the start of next line
+        ///  5 Ts = superscript
+        /// -5 Ts = subscript
+
+        #region Fields
+
+        #region _numberOfCharsToKeep
+        /// <summary>
+        /// The number of characters to keep, when extracting text.
+        /// </summary>
+        private static int _numberOfCharsToKeep = 15;
+        #endregion
+
+        #endregion
+
+
+
+        #region ExtractTextFromPDFBytes
+        /// <summary>
+        /// This method processes an uncompressed Adobe (text) object 
+        /// and extracts text.
+        /// </summary>
+        /// <param name="input">uncompressed</param>
+        /// <returns></returns>
+        public string ExtractTextFromPDFBytes(byte[] input)
+        {
+            if (input == null || input.Length == 0) return "";
+
+            try
+            {
+                string resultString = "";
+
+                // Flag showing if we are we currently inside a text object
+                bool inTextObject = false;
+
+                // Flag showing if the next character is literal 
+                // e.g. '\\' to get a '\' character or '\(' to get '('
+                bool nextLiteral = false;
+
+                // () Bracket nesting level. Text appears inside ()
+                int bracketDepth = 0;
+
+                // Keep previous chars to get extract numbers etc.:
+                char[] previousCharacters = new char[_numberOfCharsToKeep];
+                for (int j = 0; j < _numberOfCharsToKeep; j++) previousCharacters[j] = ' ';
+
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    char c = (char)input[i];
+
+                    if (inTextObject)
+                    {
+                        // Position the text
+                        if (bracketDepth == 0)
+                        {
+                            if (CheckToken(new string[] { "TD", "Td" }, previousCharacters))
+                            {
+                                resultString += "\n\r";
+                            }
+                            else
+                            {
+                                if (CheckToken(new string[] { "'", "T*", "\"" }, previousCharacters))
+                                {
+                                    resultString += "\n";
+                                }
+                                else
+                                {
+                                    if (CheckToken(new string[] { "Tj" }, previousCharacters))
+                                    {
+                                        resultString += " ";
+                                    }
+                                }
+                            }
+                        }
+
+                        // End of a text object, also go to a new line.
+                        if (bracketDepth == 0 &&
+                            CheckToken(new string[] { "ET" }, previousCharacters))
+                        {
+
+                            inTextObject = false;
+                            resultString += " ";
+                        }
+                        else
+                        {
+                            // Start outputting text
+                            if ((c == '(') && (bracketDepth == 0) && (!nextLiteral))
+                            {
+                                bracketDepth = 1;
+                            }
+                            else
+                            {
+                                // Stop outputting text
+                                if ((c == ')') && (bracketDepth == 1) && (!nextLiteral))
+                                {
+                                    bracketDepth = 0;
+                                }
+                                else
+                                {
+                                    // Just a normal text character:
+                                    if (bracketDepth == 1)
+                                    {
+                                        // Only print out next character no matter what. 
+                                        // Do not interpret.
+                                        if (c == '\\' && !nextLiteral)
+                                        {
+                                            nextLiteral = true;
+                                        }
+                                        else
+                                        {
+                                            if (((c >= ' ') && (c <= '~')) ||
+                                                ((c >= 128) && (c < 255)))
+                                            {
+                                                resultString += c.ToString();
+                                            }
+
+                                            nextLiteral = false;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    // Store the recent characters for 
+                    // when we have to go back for a checking
+                    for (int j = 0; j < _numberOfCharsToKeep - 1; j++)
+                    {
+                        previousCharacters[j] = previousCharacters[j + 1];
+                    }
+                    previousCharacters[_numberOfCharsToKeep - 1] = c;
+
+                    // Start of a text object
+                    if (!inTextObject && CheckToken(new string[] { "BT" }, previousCharacters))
+                    {
+                        inTextObject = true;
+                    }
+                }
+                return resultString;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        #endregion
+
+        #region CheckToken
+        /// <summary>
+        /// Check if a certain 2 character token just came along (e.g. BT)
+        /// </summary>
+        /// <param name="search">the searched token</param>
+        /// <param name="recent">the recent character array</param>
+        /// <returns></returns>
+        private bool CheckToken(string[] tokens, char[] recent)
+        {
+            foreach (string token in tokens)
+            {
+                if (token.Length > 1)
+                {
+                    if ((recent[_numberOfCharsToKeep - 3] == token[0]) &&
+                        (recent[_numberOfCharsToKeep - 2] == token[1]) &&
+                        ((recent[_numberOfCharsToKeep - 1] == ' ') ||
+                        (recent[_numberOfCharsToKeep - 1] == 0x0d) ||
+                        (recent[_numberOfCharsToKeep - 1] == 0x0a)) &&
+                        ((recent[_numberOfCharsToKeep - 4] == ' ') ||
+                        (recent[_numberOfCharsToKeep - 4] == 0x0d) ||
+                        (recent[_numberOfCharsToKeep - 4] == 0x0a))
+                        )
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            return false;
         }
         #endregion
     }
